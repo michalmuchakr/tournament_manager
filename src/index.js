1155cb196c8960469715fa9bfb6f10acb8df1734
Krzysztof Buszewicz <krzysztof.buszewicz@railwaymen.org>
2018-04-19 13:03:38 +0200

    add missing semicolon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import { loadTranslations, setLocale, syncTranslationWithStore } from 'react-redux-i18n';
-import translationsObject from './translations/translationsObject'
+import translationsObject from './translations/translationsObject';
 
 import MainReducer from './reducers/mainReducer';
 import App from './App';
